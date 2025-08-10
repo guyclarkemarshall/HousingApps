8d814f19e0404252f5382437bf3232b9f5d4b60d
guyclarkemarshall <82951871+guyclarkemarshall@users.noreply.github.com>
2025-08-10 08:23:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,4 +10,4 @@ st.markdown("## Social Housing Dashboards")
 st.write(
     "Interactive housing analytics, using 2024 TSM data as a proof of concept. Use the left sidebar to navigate dashboards."
 )
-st.info("Tip: This is all free and open source. The author is very bad at coding and this took under 1hr.")
+st.info("Tip: This is all free and open source. The author is not good at coding and this took under 1hr with Gen AI.")
